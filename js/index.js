9bfb48cfc3edda7fe8349124ae5d0fb233e7530e
Ahmed <ahmedaraby01023980886@gmail.com>
2023-08-29 22:16:53 +0300

    v1.0

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    center: true,
+    items: 1,
+    loop: true,
+    nav: false,
+    dots: false,
+    margin: 100
+});
+$('.next-teacher').click(() => {
+    $('.teacher-carousel').trigger('next.owl.carousel');
+})
+$('.prev-teacher').click(() => {
+    $('.teacher-carousel').trigger('prev.owl.carousel');
+})
+$('.next-student').click(() => {
+    $('.student-carousel').trigger('next.owl.carousel');
+})
+$('.prev-student').click(() => {
+    $('.student-carousel').trigger('prev.owl.carousel');
+})
